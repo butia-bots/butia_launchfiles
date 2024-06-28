bd31f49addaa17ad911cbd01ecb976ccdac8a7a1
Gabriel Dorneles <dorneles1215@gmail.com>
2024-06-28 15:13:03 -0300

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fbotathome/fbot_ws/src/butia_launchfiles
-# Build directory: /home/fbotathome/fbot_ws/src/butia_launchfiles/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "butia_launchfiles"
-PROJECT_SPACE_DIR = "/home/fbotathome/fbot_ws/src/butia_launchfiles/build/devel"
-PROJECT_VERSION = "0.0.0"
